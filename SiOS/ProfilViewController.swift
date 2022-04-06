c3a04cc8e7fb617ed349c5f6d864520a40b0387c
Paul Roumy <paul.roumy@edu.itescia.fr>
2022-04-06 15:49:52 +0200

    tttttt

--- SiOS/ProfilViewController.swift
+++ SiOS/ProfilViewController.swift
@@ -17,6 +17,7 @@ class ProfilViewController: UIViewController {
     @IBOutlet weak var numeroCarteVital: UITextField!
     @IBOutlet weak var adresseText: UITextField!
     @IBOutlet weak var validationButton: UIButton!
+    let defaults = UserDefaults.standard
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,6 +26,20 @@ class ProfilViewController: UIViewController {
     
 
     @IBAction func saveProfile(_ sender: Any) {
+        defaults.set(nameText.text, forKey: "Nom")
+        
+        defaults.set(prenomText.text, forKey: "Prenom")
+        
+        defaults.set(genreButton.selectedSegmentIndex, forKey: "genre")
+        
+        defaults.set(poidText.text, forKey: "Poid")
+        
+        defaults.set(contactNum.text, forKey: "ContactNum")
+        
+        defaults.set(numeroCarteVital.text, forKey: "NumeroCarteVital")
+        
+        defaults.set(adresseText.text, forKey: "NumeroCarteVital")
+        
     }
     /*
     // MARK: - Navigation
--- SiOS/ViewController.swift
+++ SiOS/ViewController.swift
@@ -8,9 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let defaults = UserDefaults.standard
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        if (defaults.string(forKey: "Nom") == nil){
+            
+            if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "profileId") as? ProfilViewController {
+                
+                self.present(vc, animated: true)
+            }
+            
+            
+            
+        }
         // Do any additional setup after loading the view.
     }
 
