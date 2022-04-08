d35e2b8c0e296c5ebda036907deca6dc200ffffc
Anais <anais.hammouche@edu.itescia.fr>
2022-04-08 11:54:06 +0200

    comments

--- SiOS/CallModalController.swift
+++ SiOS/CallModalController.swift
@@ -6,6 +6,8 @@ class CallModalController: UIViewController {
     @IBOutlet weak var numLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // we make the call
         let phone = "tel:" + self.text
         let url = URL(string: phone)!
         UIApplication.shared.open(url, options: [:], completionHandler: nil)
